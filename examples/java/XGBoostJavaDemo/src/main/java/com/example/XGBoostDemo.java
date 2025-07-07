10257bc311dcdbb17a59ab82bd5faae28ed9a4a9
Geremy Cohen <geremy.cohen@arm.com>
2025-07-07 10:50:19 -0700

    feat: add installation script and update README for XGBoost Java integration

--- examples/java/XGBoostJavaDemo/src/main/java/com/example/XGBoostDemo.java
+++ examples/java/XGBoostJavaDemo/src/main/java/com/example/XGBoostDemo.java
@@ -1,17 +1,31 @@
 package com.example;
 
-import ml.dmlc.xgboost4j.java.DMatrix;
-import ml.dmlc.xgboost4j.java.XGBoost;
-import ml.dmlc.xgboost4j.java.Booster;
+import ml.dmlc.xgboost4j.java.*;
+import java.util.HashMap;
+import java.io.IOException;
 
 public class XGBoostDemo {
+    static {
+        System.load("/usr/local/lib/libxgboost4j.so");
+    }
+
     public static void main(String[] args) {
         try {
-            // Simple example using dummy data
-            DMatrix trainMat = new DMatrix(new float[]{1,2,3,4}, 2, 2);
-            Booster booster = XGBoost.train(trainMat, null, 1, new java.util.HashMap<>(), null, null);
+            float[] data = new float[] {1, 2, 3, 4};
+            DMatrix trainMat = new DMatrix(data, 2, 2);
+            trainMat.setLabel(new float[] {0, 1});
+
+            HashMap<String, Object> params = new HashMap<>();
+            params.put("objective", "binary:logistic");
+            params.put("eval_metric", "logloss");
+
+            Booster booster = XGBoost.train(trainMat, params, 2, new HashMap<>(), null, null);
             System.out.println("Booster created successfully: " + booster);
-        } catch (Exception e) {
+
+            System.out.println("Press Enter key to exit...");
+            System.in.read();
+
+        } catch (XGBoostError | IOException e) {
             e.printStackTrace();
         }
     }
