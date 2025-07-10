a0d9d6cdfa390251640d16c5fb8dddca21c66608
Geremy Cohen <geremy.cohen@arm.com>
2025-07-10 14:33:10 -0700

    feat: update installation script to install OpenJDK 21 and set JAVA_HOME for building JavaFX

--- java/jfx/install.sh
+++ java/jfx/install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # 1. Install system packages
-sudo apt install -y openjdk-17-jdk openjdk-21-jdk maven git cmake build-essential python3 \
-    python3-pip python3-dev wget libgtk-3-dev libgl1-mesa-dev gradle libx11-dev libxext-dev \
-    libxrender-dev libxtst-dev libxi-dev libxrandr-dev libxcursor-dev libxss-dev libxinerama-dev \
-    libfreetype6-dev libfontconfig1-dev libasound2-dev
-sudo apt install -y openjdk-17-jdk openjdk-21-jdk maven git cmake build-essential python3 \
+sudo apt update && sudo apt install -y openjdk-17-jdk openjdk-21-jdk maven git cmake build-essential python3 \
     python3-pip python3-dev wget libgtk-3-dev libgl1-mesa-dev gradle libx11-dev libxext-dev \
     libxrender-dev libxtst-dev libxi-dev libxrandr-dev libxcursor-dev libxss-dev libxinerama-dev \
     libfreetype6-dev libfontconfig1-dev libasound2-dev
@@ -35,9 +31,9 @@ fi
 
 # 3. Clone JavaFX from source
 cd
-if [ ! -d "~/jfx" ]; then
-    git clone https://github.com/openjdk/jfx.git ~/jfx
-fi
+# Force fresh clone to avoid Gradle cache issues
+rm -rf ~/jfx
+git clone https://github.com/openjdk/jfx.git ~/jfx
 cd ~/jfx
 # Checkout JavaFX 17 branch which is compatible with Java 17
 git checkout jfx17
